b083bbfae7ed0841952357f6fd49be1ab412ae34
Daniel Paul Lin <danielin@umich.edu>
2016-03-29 13:25:07 -0400

    starting quicksort

--- algorithms/QuickSort.h
+++ algorithms/QuickSort.h
@@ -1,1 +1,17 @@
 #pragma once
+
+
+template<typenameT>
+int partition(T* array, int length)
+{
+
+	pivot_val = array[length -1];
+	int swap_index, check_index;
+}
+
+template<typename T>
+quickSort(T* array, int length)
+{
+	int pivot_index = partition(
+
+}
