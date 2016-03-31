76592aac3361ae89f4b23eb4c3f3b63d25120094
danielin917 <danielin@umich.edu>
2016-03-30 22:42:57 -0400

    binary heap: when adding fixup, when removing fixdown

--- algorithms/HeapSort.h
+++ algorithms/HeapSort.h
@@ -18,21 +18,56 @@ void fixDown(T* array, int length, int cur_index)
 		
 		if(array[next_index] > array[cur_index]){
 			referenceSwap(array[next_index], array[cur_index]);
-			fixDown(array, length - (next_index - cur_index), next_index);
+			fixDown(array, length , next_index);
 		}
-		else{
+		else{	
 			return;
 		}
 	}
 }
-
+template<typename T>
+void fixUp(T* array, int length, int cur_index)
+{
+	if(cur_index == 0)
+		return;
+	int next_index = (cur_index+1)/2 - 1;
+	if(array[next_index] < array[cur_index])
+	{
+		referenceSwap(array[next_index], array[cur_index]);
+		fixUp(array, length, next_index);
+	}
+}
 template<typename T>
 void heapify(T* array, int length)
 {
 	for(int i = 0; i < length; i++)
 	{
-		fixDown(&array[length - i - 1], i + 1, 0);
+		fixUp(array, i + 1, i);		
 	}
+}
+
+template<typename T>
+bool heapified(T* array, int length)
+{
+	for(int i = 0; i < length; i++)
+	{
+		if((i+1) * 2 - 1 < length)
+		{
+			if(array[i] < array[(i+1)*2-1]){
+				cout << array[i] << ":" << array[(i+1)*2-1];
+				return false;
+			}
+		}
+		
+		if((i +1) * 2 < length)
+		{
+			if(array[i] < array[(i+1)*2]){
+				cout << array[i] << ":" << array[(i+1)*2];
+				return false;
+			}
+		}
+	}
+	return true;
 }
 
 template<typename T>
@@ -44,5 +79,6 @@ void heapSort(T* array, int length)
 		referenceSwap(array[0], array[length - i - 1]);	
 		fixDown(array, length - i - 1, 0);	
 	}
+
 }
 
